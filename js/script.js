c380d10cc3a9edf5e45778960a748f8006389b86
Maksym Kovalenko <m.kovalenko.coder@gmail.com>
2024-06-01 23:47:05 +0300

    Added Modal

--- js/script.js
+++ js/script.js
@@ -95,4 +95,56 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline); 
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => { 
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            // modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        })
+    });
+
+    // modalTrigger.addEventListener('click', () => {
+    //     modal.classList.add('show');
+    //     modal.classList.remove('hide');
+    //     // modal.classList.toggle('show');
+    //     document.body.style.overflow = 'hidden';
+    // });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal)
+
+    // modalCloseBtn.addEventListener('click', () => {
+    //     modal.classList.add('hide');
+    //     modal.classList.remove('show');
+    //     // modal.classList.toggle('show');
+    //     document.body.style.overflow = '';
+    // });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            // modal.classList.add('hide');
+            // modal.classList.remove('show');
+            // document.body.style.overflow = '';
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
